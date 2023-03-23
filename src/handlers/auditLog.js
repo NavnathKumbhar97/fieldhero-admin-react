363adf437306fd8f7e609234ce34186de5c3d690
NavnathKumbhar97 <nkumbhar@apexaglobal.com>
2023-03-23 17:16:38 +0530

    integrating create and get api with the front end of candidate master module

--- src/handlers/auditLog.js
+++ src/handlers/auditLog.js
@@ -1,16 +1,16 @@
 // ! Pre-configured Axios Import
-import axios from "axios"
+import axios from "../helpers/axios-handler"
 
 // ! Fetch All Audit Log Section Wise
-const fetchAllAuditLog = async (sectionId, dataId) => {
+const fetchAllAuditLog = async (sectionId, dataId,headers) => {
     let auditLog = await axios
-        .get(`/api/v1/${sectionId}/audit-log/${dataId}`)
+        .get(`/api/v1/${sectionId}/audit-log/${dataId}`,headers)
         .catch((ex) => console.error(ex.toJSON()))
     return auditLog.data
 }
 
 //! Create Audit Log Section Wise 
-const addAuditLog = async (payload, sectionId, dataId) => {
+const addAuditLog = async (payload, sectionId, dataId,headers) => {
     let response = await axios
         .post(`/api/v1/${sectionId}/audit-log/${dataId}`, {
             userName: payload.userName,
@@ -20,7 +20,7 @@ const addAuditLog = async (payload, sectionId, dataId) => {
             operationName: payload.operationName,
             sectionNameId: payload.sectionId,
             sectionDataId: payload.dataId,
-        })
+        },headers)
         .catch((ex) => console.error(ex.toJSON()))
     if (response != null) {
         if (response.status >= 200 && response.status < 300) return true
@@ -31,9 +31,9 @@ const addAuditLog = async (payload, sectionId, dataId) => {
 }
 
 // ! Fetch Single Audit Log By Id
-const fetchAuditLogById = async (id) => {
+const fetchAuditLogById = async (id,headers) => {
     let getAuditLog = await axios
-        .get(`/api/v1/audit-log/${id}`)
+        .get(`/api/v1/audit-log/${id}`,headers)
         .catch((ex) => console.error(ex.toJSON()))
     return getAuditLog.data
 }
--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,3 +1,4 @@
 import user from "./user";
+import {auditLog} from "./auditLog"
 
-export default { user}
+export default { user,auditLog}
